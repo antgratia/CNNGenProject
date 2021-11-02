39e51036b1700d80d520aae7519ff6be46eac0c1
AntoineGr1 <antoine.gratia@student.unamur.be>
2021-11-02 14:14:58 +0100

    creation class Dropout

---
+++ stateMachineLanguage/src/models/Dropout.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Dropout implements LayerInterface {
+
+	private int dropoutRate;
+	
+	
+	@Override
+	public LayerInterface getHyperparameters() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- stateMachineLanguage/src/utils/GestionHpp.java
+++ stateMachineLanguage/src/utils/GestionHpp.java
@@ -10,11 +10,28 @@ import models.Pooling;
 
 public class GestionHpp {
 	
+	// info img
+	private int maxSizeImg;
+	private int currentSizeImg;
+	
+	
+	// hpp Convolution + Pooling
 	private static List<Integer> kernel = new ArrayList<Integer>(List.of(1,2,3,4,5,6,7));
 	private static List<Integer> stride = new ArrayList<Integer>(List.of(1,2,3,4,5,6,7));
 	private static List<String> padding = new ArrayList<String>(List.of("same","valid"));
+		// hpp Convolution supp
 	private static List<String> fctActivation = new ArrayList<String>(List.of("relu","selu","tanh"));
-
+	private int nbFilter;
+	
+	// hpp dropout
+	private double dropoutRate;
+	
+	//hpp batch Normalisation
+	private double epsilon;
+	
+	// hpp Dense
+	private int units;
+	
 	public static void gestionKernelPaddingStride(LayerInterface layer) throws Exception {
 		if (layer instanceof Convolution) {
 			Convolution conv = (Convolution) layer;
